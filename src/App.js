3000f13250fca8b953a318c65339b1953da2ce17
elder-tome <r14.elderalves@gmail.com>
2020-08-06 21:14:36 -0300

    insert number in buttons

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState } from 'react';
 import { MaterialIcons } from '@expo/vector-icons';
 import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
 
@@ -7,58 +7,83 @@ import Logo from './assets/Logo.png';
 
 
 export default function App() {
+
+  const [ ap1, setAp1 ] = useState('');
+  const [ ap2, setAp2 ] = useState('');
+  const [ ap3, setAp3 ] = useState('');
+
+  const [ apSelected, setApSelected ] = useState('');
+
+  console.log(apSelected);
+  console.log(`ap1: ${ap1}`);
+  console.log(`ap2: ${ap2}`);
+  console.log(`ap3: ${ap3}`);
+
+  function insertNumber(textfield, number){
+    if(textfield === 'ap1'){
+      setAp1(ap1 + number);
+    }
+    if(textfield === 'ap2'){
+      setAp2(ap2 + number);
+    }
+    if(textfield === 'ap3'){
+      setAp3(ap3 + number);
+    }
+  }
+
   return (
     <View style={styles.container}>
       <StatusBar style="light"/>
       <View style={styles.mainContainer}>
         <Image source={Logo}/>
         <View style={styles.inputsContainer}>
-          <TouchableOpacity style={styles.ButtonAP}>
-            <Text style={ styles.textButton }>AP1</Text>
+          <TouchableOpacity style={styles.ButtonAP} onPress={() => setApSelected('ap1')}>
+            <Text style={ styles.textButton }>{ap1}</Text>
           </TouchableOpacity>
-          <TouchableOpacity style={styles.ButtonAP}>
-            <Text style={ styles.textButton }>AP2</Text>
+          <TouchableOpacity style={styles.ButtonAP} onPress={() => setApSelected('ap2')}>
+            <Text style={ styles.textButton }>{ap2}</Text>
           </TouchableOpacity>
-          <TouchableOpacity style={styles.ButtonAP}>
-            <Text style={ styles.textButton }>AP3</Text>
+          <TouchableOpacity style={styles.ButtonAP} onPress={() => setApSelected('ap3')}>
+            <Text style={ styles.textButton }>{ap3}</Text>
           </TouchableOpacity>
         </View>
-        <Text style={ styles.text }>Parabéns sua média foi 7.6</Text>
+        <Text style={ styles.text }>Parabéns sua média foi 8.5</Text>
       </View>
       <View style={styles.keyboardContainer} >
-        <TouchableOpacity style={ styles.button } onPress={() => {}}>
+        <TouchableOpacity style={ styles.button } onPress={() => insertNumber(apSelected, '1')}>
           <Text style={ styles.textButton }>1</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={ styles.button } onPress={() => {}}>
+        <TouchableOpacity style={ styles.button } onPress={() => insertNumber(apSelected, '2')}>
           <Text style={ styles.textButton }>2</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={ styles.button } onPress={() => {}}>
+        <TouchableOpacity style={ styles.button } onPress={() => insertNumber(apSelected, '3')}>
           <Text style={ styles.textButton }>3</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={ styles.button } onPress={() => {}}>
+        <TouchableOpacity style={ styles.button } onPress={() => insertNumber(apSelected, '4')}>
           <Text style={ styles.textButton }>4</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={ styles.button } onPress={() => {}}>
+        <TouchableOpacity style={ styles.button } onPress={() => insertNumber(apSelected, '5')}>
           <Text style={ styles.textButton }>5</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={ styles.button } onPress={() => {}}>
+        <TouchableOpacity style={ styles.button } onPress={() => insertNumber(apSelected, '6')}>
           <Text style={ styles.textButton }>6</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={ styles.button } onPress={() => {}}>
+        <TouchableOpacity style={ styles.button } onPress={() => insertNumber(apSelected, '7')}>
           <Text style={ styles.textButton }>7</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={ styles.button } onPress={() => {}}>
+        <TouchableOpacity style={ styles.button } onPress={() => insertNumber(apSelected, '8')}>
           <Text style={ styles.textButton }>8</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={ styles.button } onPress={() => {}}>
+        <TouchableOpacity style={ styles.button } onPress={() => insertNumber(apSelected, '9')}>
           <Text style={ styles.textButton }>9</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={ styles.button } onPress={() => {}}>
+        <TouchableOpacity style={ styles.button } onPress={() => insertNumber(apSelected, '.')}>
           <Text style={ styles.textButton }>.</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={ styles.button } onPress={() => {}}>
+        <TouchableOpacity style={ styles.button } onPress={() => insertNumber(apSelected, '0')}>
           <Text style={ styles.textButton }>0</Text>
         </TouchableOpacity>
+        
         <TouchableOpacity style={ styles.buttonDone } onPress={() => {}}>
           <MaterialIcons  name='done' size={30} color='#000'/>
         </TouchableOpacity>
